6277b25327bef9f3d85bc36b7cfbc64883af29a2
niki03petrov <78559158+niki03petrov@users.noreply.github.com>
2024-02-25 20:02:43 +0200

    minor changes

--- test/bg/sofia/uni/fmi/mjt/splitwise/command/CommandExecutorTest.java
+++ test/bg/sofia/uni/fmi/mjt/splitwise/command/CommandExecutorTest.java
@@ -1,6 +1,7 @@
 package bg.sofia.uni.fmi.mjt.splitwise.command;
 
 import bg.sofia.uni.fmi.mjt.splitwise.command.currency.client.ExchangeRate;
+import bg.sofia.uni.fmi.mjt.splitwise.streams.ReaderWriterCreator;
 import bg.sofia.uni.fmi.mjt.splitwise.user.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import java.net.http.HttpClient;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -41,14 +43,6 @@ public class CommandExecutorTest {
         assertTrue(
             executor.execute(command, user, client)
                 .contains("create-group <group_name> <username> <username> ... <username>"));
-    }
-
-    @Test
-    void testExecuteUnknown() {
-        when(command.args()).thenReturn(new String[] {"unknown"});
-
-        assertNotNull(command.args());
-        assertEquals(executor.execute(command, user, client), "Unknown command");
     }
 
 }
