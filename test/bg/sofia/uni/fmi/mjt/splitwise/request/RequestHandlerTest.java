7e71a67a1180f4c0d2489771591a0d75178d5a13
niki03petrov <78559158+niki03petrov@users.noreply.github.com>
2026-04-10 17:09:57 +0300

    minor changes

--- test/bg/sofia/uni/fmi/mjt/splitwise/request/RequestHandlerTest.java
+++ test/bg/sofia/uni/fmi/mjt/splitwise/request/RequestHandlerTest.java
@@ -1,14 +1,17 @@
 package bg.sofia.uni.fmi.mjt.splitwise.request;
 
+import bg.sofia.uni.fmi.mjt.splitwise.client.request.Request;
 import bg.sofia.uni.fmi.mjt.splitwise.command.CommandRegistry;
 import bg.sofia.uni.fmi.mjt.splitwise.server.SessionsManager;
 import bg.sofia.uni.fmi.mjt.splitwise.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RequestHandlerTest {
     private RequestHandler requestHandler;
+    private Request request;
 
     @BeforeEach
     void setUp() {
@@ -16,7 +19,6 @@ public class RequestHandlerTest {
         SessionsManager sessionsManager = mock();
         UserService userService = mock();
         requestHandler = new RequestHandler(commandRegistry, sessionsManager, userService);
-
     }
 
 }
