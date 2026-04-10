12394616c25d39b1e3e358ad3b84c8032652184c
niki03petrov <78559158+niki03petrov@users.noreply.github.com>
2026-04-10 17:06:51 +0300

    dependencies corrected, unit tests started

--- test/bg/sofia/uni/fmi/mjt/splitwise/request/RequestHandlerTest.java
+++ test/bg/sofia/uni/fmi/mjt/splitwise/request/RequestHandlerTest.java
@@ -1,5 +1,22 @@
 package bg.sofia.uni.fmi.mjt.splitwise.request;
 
+import bg.sofia.uni.fmi.mjt.splitwise.command.CommandRegistry;
+import bg.sofia.uni.fmi.mjt.splitwise.server.SessionsManager;
+import bg.sofia.uni.fmi.mjt.splitwise.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.mockito.Mockito.mock;
+
 public class RequestHandlerTest {
+    private RequestHandler requestHandler;
+
+    @BeforeEach
+    void setUp() {
+        CommandRegistry commandRegistry = mock();
+        SessionsManager sessionsManager = mock();
+        UserService userService = mock();
+        requestHandler = new RequestHandler(commandRegistry, sessionsManager, userService);
+
+    }
 
 }
